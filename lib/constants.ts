c05501a6c9ca13d8b65d5f9d242ebeafa2eed6b6
cianurosodico <jccanchilas@unal.edu.co>
2023-06-28 08:07:59 -0500

    fixes2

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,90 +19,136 @@ interface Token {
 }
 
 export const POLYGON_TOKENS: Token[] = [
-  
   {
-    chainId: 56,
-    name: "Alquimia Token",
-    symbol: "ALQ",
+    chainId: 137,
+    name: "Wrapped Matic",
+    symbol: "WMATIC",
     decimals: 18,
-    address: "0x4b48c0db4e460c894bfc031d602a5c3b57a26857",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ALQsvg.svg ",
+    address: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+    logoURI:
+      "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/polygon/assets/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270/logo.png",
   },
   {
-    chainId: 56,
-    name: "OGT",
-    symbol: "OGT",
+    chainId: 137,
+    name: "Dai - PoS",
+    symbol: "DAI",
     decimals: 18,
-    address: "0x598642f59c0366643c6f9ee3252cbb3ef1524c51",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ogt.svg",
+    address: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/dai.svg",
   },
   {
-    chainId: 56,
-    name: "Tether USD ",
+    chainId: 137,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/usdc.svg",
+  },
+  {
+    chainId: 137,
+    name: "Uniswap",
+    symbol: "UNI",
+    decimals: 18,
+    address: "0xb33eaad8d922b1083446dc23f610c2567fb5180f",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/uni.svg",
+  },
+  {
+    chainId: 137,
+    name: "Tether USD - PoS",
     symbol: "USDT",
-    decimals: 18,
-    address: "0x55d398326f99059ff775485246999027b3197955",
+    decimals: 6,
+    address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
     logoURI:
       "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdt.svg",
   },
 ];
 
 export const POLYGON_TOKENS_BY_SYMBOL: Record<string, Token> = {
-  
-  alq: {
-    chainId: 56,
-    name: "Alquimia Token",
-    symbol: "ALQ",
+  wmatic: {
+    chainId: 137,
+    name: "Wrapped Matic",
+    symbol: "WMATIC",
     decimals: 18,
-    address: "0x4b48c0db4e460c894bfc031d602a5c3b57a26857",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ALQsvg.svg ",
+    address: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+    logoURI:
+      "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/polygon/assets/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270/logo.png",
   },
-  ogt: {
-    chainId: 56,
-    name: "OGT",
-    symbol: "OGT",
+  dai: {
+    chainId: 137,
+    name: "Dai - PoS",
+    symbol: "DAI",
     decimals: 18,
-    address: "0x598642f59c0366643c6f9ee3252cbb3ef1524c51",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ogt.svg",
+    address: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/dai.svg",
+  },
+  usdc: {
+    chainId: 137,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/usdc.svg",
+  },
+  uni: {
+    chainId: 137,
+    name: "Uniswap",
+    symbol: "UNI",
+    decimals: 18,
+    address: "0xb33eaad8d922b1083446dc23f610c2567fb5180f",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/uni.svg",
   },
   usdt: {
-    chainId: 56,
-    name: "Tether USD ",
+    chainId: 137,
+    name: "Tether USD - PoS",
     symbol: "USDT",
-    decimals: 18,
-    address: "0x55d398326f99059ff775485246999027b3197955",
+    decimals: 6,
+    address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
     logoURI:
       "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdt.svg",
   },
 };
 
 export const POLYGON_TOKENS_BY_ADDRESS: Record<string, Token> = {
-  
-  "0x4b48c0db4e460c894bfc031d602a5c3b57a26857": {
-    chainId: 56,
-    name: "Alquimia Token",
-    symbol: "ALQ",
+  "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270": {
+    chainId: 137,
+    name: "Wrapped Matic",
+    symbol: "WMATIC",
     decimals: 18,
-    address: "0x4b48c0db4e460c894bfc031d602a5c3b57a26857",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ALQsvg.svg ",
+    address: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+    logoURI:
+      "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/polygon/assets/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270/logo.png",
   },
-  "0x598642f59c0366643c6f9ee3252cbb3ef1524c51": {
-    chainId: 56,
-    name: "OGT",
-    symbol: "OGT",
+  "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063": {
+    chainId: 137,
+    name: "DAI - PoS",
+    symbol: "DAI",
     decimals: 18,
-    address: "0x598642f59c0366643c6f9ee3252cbb3ef1524c51",
-    logoURI: "https://raw.githubusercontent.com/Cianurosodico/logos/main/ogt.svg",
+    address: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/dai.svg",
   },
-  "0x55d398326f99059ff775485246999027b3197955": {
-    chainId: 56,
-    name: "Tether USD",
+  "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": {
+    chainId: 137,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/usdc.svg",
+  },
+  "0xb33eaad8d922b1083446dc23f610c2567fb5180f": {
+    chainId: 137,
+    name: "Uniswap",
+    symbol: "UNI",
+    decimals: 18,
+    address: "0xb33eaad8d922b1083446dc23f610c2567fb5180f",
+    logoURI: "https://wallet-asset.matic.network/img/tokens/uni.svg",
+  },
+  "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": {
+    chainId: 137,
+    name: "Tether USD - PoS",
     symbol: "USDT",
-    decimals: 18,
-    address: "0x55d398326f99059ff775485246999027b3197955",
+    decimals: 6,
+    address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
     logoURI:
       "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdt.svg",
   },
 };
-
-
